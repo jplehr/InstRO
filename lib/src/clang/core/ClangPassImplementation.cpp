c2c33bbe291711c5e66c5215e00e14f548fc72eb
Jan Lehr <jp.lehr@gmail.com>
2019-01-30 10:46:23 +0100

    Removed an unused source file.

--- lib/src/clang/core/ClangPassImplementation.cpp
+++
@@ -1,7 +0,0 @@
-#include "instro/clang/core/ClangAdapterPass.h"
-
-void InstRO::Clang::Core::ClangPassImplementation::execute() {
-	assert(executer != nullptr);
-	std::cout << "Executing inside ClangPassImplementation" << std::endl;
-	executer->execute(this);
-}
